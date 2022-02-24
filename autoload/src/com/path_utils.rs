1f910a44badd45410f96df571b408aa332f67d66
Joinz <450220020@qq.com>
2022-02-24 22:25:40 +0800

    Update path_utils.rs
    test path

--- autoload/src/com/path_utils.rs
+++ autoload/src/com/path_utils.rs
@@ -1,8 +1,8 @@
 use std::{path::Path};
 
 pub fn get_auto_starter_path(){
-    let path = Path::new("../libs");
-    println!("asb_path:{:?}", path.canonicalize().unwrap());
+    let path = Path::new("../");
+    println!("asb_path:{:?}", path.canonicalize());
 
     let path_a = env!("CARGO_MANIFEST_DIR");
     println!("mat_path:{:?}",path_a);
